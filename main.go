22f0b85adf9b9cc0f01b7747187b4ebb716556a7
龙军 <570580380@qq.com>
2022-07-12 09:52:33 +0800

    基本的目录

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		return
+	} // 监听0.0.0.0:8080启动服务
+}
